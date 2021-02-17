ed3a38c2c133f19a84a944f331f5a07524bfccbf
Toriku <anatoly.kravtsov@gmail.com>
2021-02-17 13:15:01 +0500

    feat: add min, max, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
 exports.min = function min (array) {
+  if (array && array.length > 0){
+    return Math.min(...array);
+  } else {
   return 0;
+  }
 }
 
 exports.max = function max (array) {
+  if (array && array.length > 0){
+    return Math.max(...array);
+  } else {
   return 0;
+  }
 }
 
 exports.avg = function avg (array) {
+  if (array && array.length > 0){
+    let sum = 0;
+    for (number of array){
+sum += number
+    }
+    return sum / array.length;
+  } else {
   return 0;
+  }
 }
